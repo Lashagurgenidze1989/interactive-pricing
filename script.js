5f32e65b52a35ce54c95275731385d34e9d15e4d
Lashagurgenidze1989 <lashagurgenidze1989@gmail.com>
2023-10-28 11:41:11 -0600

    add some styles

--- script.js
+++ script.js
@@ -4,51 +4,37 @@ const cost = document.querySelector(".cost");
 const range = document.querySelector("#range");
 const discountCheck = document.querySelector("#checkbox");
 
-console.log(Number(range.value));
+const monthly_yearly = document.querySelector(".monthly");
+const leftWhite = document.querySelector(".leftWhite");
+const rightWhite = document.querySelector(".rightWhite");
 
 range.addEventListener("input", () => generator(event));
 
 function generator(event) {
   if (Number(event.target.value) === 8) {
     views.innerHTML = "10K pageviews";
-  }
-
-  if (Number(event.target.value) === 12) {
+  } else if (Number(event.target.value) === 12) {
     views.innerHTML = "50K pageviews";
-  }
-
-  if (Number(event.target.value) === 16) {
+  } else if (Number(event.target.value) === 16) {
     views.innerHTML = "75K pageviews";
-  }
-
-  if (Number(event.target.value) === 20) {
+  } else if (Number(event.target.value) === 20) {
     views.innerHTML = "100K pageviews";
-  }
-
-  if (Number(event.target.value) === 24) {
+  } else if (Number(event.target.value) === 24) {
     views.innerHTML = "500K pageviews";
-  }
-
-  if (Number(event.target.value) === 28) {
+  } else if (Number(event.target.value) === 28) {
     views.innerHTML = "750K pageviews";
-  }
-
-  if (Number(event.target.value) === 32) {
+  } else if (Number(event.target.value) === 32) {
     views.innerHTML = "1M pageviews";
   }
-
   cost.textContent = "$" + Number(event.target.value).toFixed(2);
 
-  /* if (discountCheck.checked) {
-    event.target.min = "72";
-    event.target.max = "288";
-    event.target.step = "36";
+  let value = event.target.value;
+  let min = event.target.min;
+  let max = event.target.max;
 
-    event.target.value = 100;
+  let percentage = ((value - min) / (max - min)) * 100 + "%";
 
-    Number(event.target.value) * 12 -
-      (Number(event.target.value) * 12 * 25) / 100;
-  } */
+  document.documentElement.style.setProperty("--thumb-position", percentage);
 }
 
 discountCheck.addEventListener("change", (event) => {
@@ -59,5 +45,16 @@ discountCheck.addEventListener("change", (event) => {
     console.log(range.value);
     range.value = (Number(range.value) * 12 * 75) / 100;
     console.log(range.value);
+
+    cost.innerHTML = `$${range.value.toFixed(2)}`;
+    monthly_yearly.innerHTML = "/ year";
+
+    leftWhite.style.display = "none";
+    rightWhite.style.display = "block";
+  } else {
+    cost.innerHTML = `$${range.value.toFixed(2)}`;
+    monthly_yearly.innerHTML = "/ month";
+    leftWhite.style.display = "block";
+    rightWhite.style.display = "none";
   }
 });
